153dd904b7391c31dc319b65f08d5b03e44dc50c
chetanallannavar <chetanallannavar21@gmail.com>
2021-03-19 07:43:29 +0530

    Fetch Controller is added

---
+++ src/main/java/com/example/microservice1/microservice1/controller/FetchDataController.java
@@ -0,0 +1,18 @@
+package com.example.microservice1.microservice1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@RestController
+public class FetchDataController {
+
+	@GetMapping("/fetchdata")
+	public LimitConfiguration getdata() {
+		RestTemplate template = new RestTemplate();
+		LimitConfiguration forObject = template.getForObject("http://localhost:8080/limits", LimitConfiguration.class);
+		return forObject;
+	}
+
+}
